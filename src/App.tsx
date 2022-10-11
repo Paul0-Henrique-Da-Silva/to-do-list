52657d13cbe24357e6f22b9ebaca4fbb1f5ddbf4
Paulo Henrique <ph46163835@gmail.com>
2022-10-10 22:19:50 -0300

    feat: style main e tag main

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
+import styles from './App.module.css'
+
 function App() {
   return (
     <div>
       <Header />
-      <h1>Conteudo...</h1>
+      <main className={styles.main}>
+        <h1>Conteudo...</h1>
+      </main>
       <Footer />
     </div>
   )
