fb388a3974201258ae1dda4602b999804876ec04
Paulo Henrique <ph46163835@gmail.com>
2022-10-10 23:26:11 -0300

    feat: interface task

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, {useState, ChangeEvent, FormEvent, useEffect} from "react";
 import styles from "./TaskForm.module.css"
+
+import { ITask } from "../interfaces/task"
 
 interface Props {
     btnText: string
 }
 
 export const TaskForm = ({btnText}: Props) => {
+    
+    const [id, setId] = useState<number>(0)
+    const [text, setText] = useState<string>("")
+    const [level, setLevel] = useState<number>(0)
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if(e.target.name === "title") 
+            { setText(e.target.value)}
+        else { setLevel(parseInt(e.target.value))}    
+    }
+
+    const addTaskHandle = () => {
+     
+    }
+
     return(
-       <form className={styles.form}>
+       <form onSubmit={addTaskHandle} className={styles.form}>
             <div className={styles.input_container}>
                 <label htmlFor="title">Título:</label>
-                <input type="text" name="title" placeholder="Título da tarefa"/>
+                <input type="text" name="title" 
+                placeholder="Título da tarefa" onChange={handleChange}/>
             </div>
             <div className={styles.input_container}>
                 <label htmlFor="level">Dificuldade:</label>
-                <input type="text" name="level" placeholder="Dificuldade da tarefa"/>
+                <input type="text" name="level" 
+                placeholder="Dificuldade da tarefa" onChange={handleChange}/>
             </div>
             <input type="submit" value={btnText}/>
        </form>     
