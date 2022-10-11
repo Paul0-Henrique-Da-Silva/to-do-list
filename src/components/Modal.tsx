4f4a725bb1ad2451712bdadd579e2ceec9a81043
Paulo Henrique <ph46163835@gmail.com>
2022-10-11 20:03:40 -0300

    feat: closeModal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,9 +6,15 @@ interface Props {
 }
 
 export const Modal = ({ children }: Props) => {
+  
+  const closeModal = (e: React.MouseEvent): void => {
+    const modal = document.querySelector("#modal")
+    modal!.classList.add("hide")
+  }
+
   return (
     <div id="modal">
-        <div className={styles.fade}></div>
+        <div className={styles.fade} onClick={closeModal}></div>
         <div className={styles.modal}>  
             {children}
         </div>
